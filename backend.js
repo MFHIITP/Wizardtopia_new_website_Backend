06aa31d6220a31d8e3c7c2ff85af9ce36f01a441
Farshid <hossainfarshid@gmail.com>
2024-06-05 15:53:26 +0530

    Fourth commit

--- backend.js
+++ backend.js
@@ -90,6 +90,45 @@ app.post('/backend_posts', async (req, res)=>{
 })
 
 
+let points_schema = mongoose.Schema({
+    house: String,
+    points: Number
+})
+
+const pointcollection = mongoose.model('points', points_schema)
+
+app.get('/backend_points', async(req, res)=>{
+    const gryf = await pointcollection.find({house: "Gryffindor"})
+    const raven = await pointcollection.find({house: "Ravenclaw"})
+    const huff = await pointcollection.find({house: "Huffelpuff"})
+    const slyth = await pointcollection.find({house: "Slytherin"})
+    res.status(200).json({
+        Gryffindor: gryf[0].points,
+        Ravenclaw: raven[0].points,
+        Huffelpuff: huff[0].points,
+        Slytherin: slyth[0].points
+    })
+})
+
+app.post('/backend_points_increase', async(req, res)=>{
+    const oldpoint = await pointcollection.find({house: req.body.house})
+    let newpoint = req.body.point + oldpoint[0].points
+    await pointcollection.updateOne({house: req.body.house}, {$set:{points: newpoint}})
+    res.status(200).json({
+        newpoint: newpoint,
+    })
+})
+
+app.post('/backend_points_decrease', async(req, res)=>{
+    const oldpoint = await pointcollection.find({house: req.body.house})
+    let newpoint = req.body.point - oldpoint[0].points
+    await pointcollection.updateOne({house: req.body.house}, {$set:{points: newpoint}})
+    res.status(200).json({
+        newpoint: newpoint,
+    })
+})
+
+
 app.listen(port, hostname, ()=>{
     console.log(`Server Listning ${port}`)
 })
